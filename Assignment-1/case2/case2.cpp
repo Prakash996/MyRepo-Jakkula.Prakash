4388e61cf30b18e19c0a4312acf13ae0a25e4eac
Prakash996 <50233483+Prakash996@users.noreply.github.com>
2020-04-12 18:00:59 +0530

    Update case2.cpp

--- Assignment-1/case2/case2.cpp
+++ Assignment-1/case2/case2.cpp
@@ -1,10 +1,11 @@
 /*
 filename: case2.cpp
-details: Program to find the formula for give expression.  Expression: 1+4+9+16+….+100
-author:J.P.Prakash
-date:07/04/2020
+details: Program to find the formula for give expression.  Expression: 1+4+9+16+â€¦.+100
+author: J.P.Prakash
+date: 07/04/2020
 */
 #include<iostream>
+#include<string.h>
 #include<stdlib.h>
 using namespace std;
 
@@ -12,28 +13,33 @@ using namespace std;
 int main(int argc,char *argv[])
 {
     int iSum,iNumb; //declaring integer variables
-    if(argc==1) 
+	if(argc==2)
 	{
-		cout<<"\n Usage of file --> \n"
-		"\t filename.exe argument"<<endl<<
-		"			or"<<endl<<
-		"\t ./filename.out argument"<<endl;
-		
-		cout<<"Enter Input: ";
-		cin>>iNumb; //read 'iNumb'
-		iSum=0;
-    	for(int i=1; i<=iNumb; ++i)	//checks the argument value
-        	iSum += (i*i);	//gives output based on the expression: 1+4+9+16+….+100
-     	cout << "Output: " << iSum;
-     	return 0;
+		if(strcmp(argv[1],"-h")==0)     //created a help command
+        	{
+			cout<<"\n Usage of file --> \n"
+					"\t filename.exe argument"<<endl<<
+					"		or"<<endl<<
+					"\t ./filename.out argument"<<endl;
+		}	
+		else
+		{	
+			iNumb = atoi(argv[1]); //assigning the argument to 'n'
+	    		iSum=0;
+	    		for(int i=1; i<=iNumb; ++i)	//checks the argument value
+	        		iSum += (i*i);	//gives output based on the expression: 1+4+9+16+â€¦.+100
+	     		cout << "Output: " << iSum;
+	     		return 0;
+		} 
 	}
 	else
 	{	
-		iNumb = atoi(argv[1]); //assigning the argument to 'n'
-    	iSum=0;
-    	for(int i=1; i<=iNumb; ++i)	//checks the argument value
-        	iSum += (i*i);	//gives output based on the expression: 1+4+9+16+….+100
-     	cout << "Output: " << iSum;
-     	return 0;
+		cout<<"Enter Input: ";
+		cin>>iNumb; //read 'iNumb'
+		iSum=0;
+    		for(int i=1; i<=iNumb; ++i)	//checks the argument value
+        		iSum += (i*i);	//gives output based on the expression: 1+4+9+16+â€¦.+100
+     		cout << "Output: " << iSum;
+     		return 0;
 	} 
 }
