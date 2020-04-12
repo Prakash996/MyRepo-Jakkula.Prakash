940300f578163b904ee10dd4704c6db9e98c9893
Prakash996 <50233483+Prakash996@users.noreply.github.com>
2020-04-12 19:10:47 +0530

    Update case4.cpp

--- Assignment-1/case4/case4.cpp
+++ Assignment-1/case4/case4.cpp
@@ -59,10 +59,10 @@ class Sub_class: public Base
 // main function
 int main(int argc,char *argv[])
 {
-    if(argc==2) 
+    	if(argc==2) 
 	{
 		if(strcmp(argv[1],"-h")==0)     //created a help command
-        {
+        	{
 			cout<<"\n Usage of file --> \n"
 					"\t filename.exe & enter"<<endl<<
 					" Program used to demonstrate different access specifiers "<<endl;
