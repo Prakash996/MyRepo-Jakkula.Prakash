0967db0e5c59b41047a1c38eeb3d54c42a21deb9
Prakash996 <50233483+Prakash996@users.noreply.github.com>
2020-04-24 01:06:42 +0530

    Update case2.cpp

--- Assignment-1/case2/case2.cpp
+++ Assignment-1/case2/case2.cpp
@@ -12,7 +12,7 @@ using namespace std;
 //main function
 int main(int argc,char *argv[])
 {
-    int iSum,iNumb; //declaring integer variables
+    int iNumb, iSum; //declaring integer variables
 	if(argc==2)
 	{
 		if(strcmp(argv[1],"-h")==0)     //created a help command
@@ -22,24 +22,18 @@ int main(int argc,char *argv[])
 					"		or"<<endl<<
 					"\t ./filename.out argument"<<endl;
 		}	
-		else
-		{	
-			iNumb = atoi(argv[1]); //assigning the argument to 'n'
-	    		iSum=0;
-	    		for(int i=1; i<=iNumb; ++i)	//checks the argument value
-	        		iSum += (i*i);	//gives output based on the expression: 1+4+9+16+….+100
-	     		cout << "Output: " << iSum;
-	     		return 0;
-		} 
 	}
 	else
 	{	
 		cout<<"Enter Input: ";
 		cin>>iNumb; //read 'iNumb'
 		iSum=0;
-    		for(int i=1; i<=iNumb; ++i)	//checks the argument value
-        		iSum += (i*i);	//gives output based on the expression: 1+4+9+16+….+100
-     		cout << "Output: " << iSum;
-     		return 0;
+		for(int i=1; i<=iNumb; ++i)	//checks the argument value
+	 		iSum += (i*i);	//gives output based on the expression: 1+4+9+16+….+100
+		if(iSum==0)
+	    		cout<<"\n Invalid (user may have entered Zero / negative values / alphabet / special characters) \n";
+	    	else
+	    		cout << "Output: " << iSum;
 	} 
+	return 0;
 }
